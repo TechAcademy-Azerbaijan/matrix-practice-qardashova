08f86171d5752508b6777df3bf81a0b2abc638b4
qardashova <58748372+qardashova@users.noreply.github.com>
2022-06-17 10:10:40 +0300

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -1,10 +1,21 @@
-// Use this matrix for solution
-
-let matrix = [
-    [2, 5, 3, 1, 4],
-    [3, 5, 2, 3, 4],
-    [4, 6, 1, 2, 3],
-    [4, 5, 6, 7, 7]
-]
-
-// Write code here. 
+      let matrix = [
+        [2, 5, 3, 1, 4],
+        [3, 5, 2, 3, 4],
+        [4, 6, 1, 2, 3],
+        [4, 5, 6, 7, 7]
+    ]
+    var min = matrix[0][0];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if(matrix[i][j] < min){
+          min = matrix[i][j];
+        }
+      } 
+    }
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix.length; j++) {
+        if(matrix[i][j] == min){
+          console.log(i+1);
+        }
+      }
+    }
